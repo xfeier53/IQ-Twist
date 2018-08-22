ece1b66ea6183511102d9e1843d14762da062896
Lachlan McVicar <u6406312@anu.edu.au>
2018-08-22 19:13:17 +1000

    git test cindy 2

--- tests/gittest/A.java
+++ tests/gittest/A.java
@@ -1,9 +1,7 @@
 package gittest;
 
 public class A {
-
-    @Override
-    public String toString() {
-        return super.toString();
+    public static void main(String[] args) {
+        A a = new A();
     }
 }
