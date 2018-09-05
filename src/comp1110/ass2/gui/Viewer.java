c30a7c30ca4e2cc45e7ad9acff22fbf9f7ad20f6
James <u6079329@anu.edu.au>
2018-09-05 21:09:32 +1000

    Drag and snap to added, moved functionality in Viewer to Board

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -1,5 +1,7 @@
 package comp1110.ass2.gui;
 
+import comp1110.ass2.Peg;
+import comp1110.ass2.Piece;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -29,7 +31,7 @@ import javafx.scene.shape.Line;
 public class Viewer extends Application {
 
     /* board layout */
-    private static final int SQUARE_SIZE = 90;
+    private static final int SQUARE_SIZE = 60;
     private static final int VIEWER_WIDTH = 750;
     private static final int VIEWER_HEIGHT = 500;
 
@@ -41,6 +43,10 @@ public class Viewer extends Application {
     private final Group pieces = new Group();
     private final Group lines = new Group();
     TextField textField;
+
+    private double[] relativeMouseClick = new double[2];
+
+
 
 
     /**
@@ -66,11 +72,21 @@ public class Viewer extends Application {
         // FIXME Task 4: implement the simple placement viewer
     }
 
+
+
     void makePiecePlacement(String piecePlacement){
+
 
         //get the id of the piece ie 'a' and get the png image
         char pieceId = piecePlacement.charAt(0);
         Image pieceImg = new Image("comp1110/ass2/gui/assets/"+pieceId+".png");
+
+        //get the height of the piece
+        double height = pieceImg.getHeight() / 100;
+        double width = pieceImg.getWidth() / 100;
+
+        //get the piece resized to suit the board
+        pieceImg = new Image("comp1110/ass2/gui/assets/"+pieceId+".png",SQUARE_SIZE * width,SQUARE_SIZE * height,true,false);
 
         //set the the image to an imageView
         ImageView pieceView = new ImageView();
@@ -86,30 +102,32 @@ public class Viewer extends Application {
         pieceView.setX(pieceView.getX() + (SQUARE_SIZE * column));
         pieceView.setY(pieceView.getY() + (SQUARE_SIZE * row));
 
-        float adjust = (float)SQUARE_SIZE / 100;
-
-        pieceView.setScaleY(adjust);
-        pieceView.setScaleX(adjust);
-
-        pieceView.setX(pieceView.getX() - (pieceView.getImage().getWidth() * (1-adjust)/2));
-        pieceView.setY(pieceView.getY() - (pieceView.getImage().getHeight() * (1-adjust)/2));
-
         //parses the orientation into an int
         int orientation = Character.getNumericValue(piecePlacement.charAt(3));
 
         rotateAndFlip(pieceView,orientation);
 
+        //Find where the piece was clicked on and store it
         pieceView.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                System.out.println(pieceId);
+                relativeMouseClick[0] = event.getX() - pieceView.getX();
+                relativeMouseClick[1] = event.getY() - pieceView.getY();
             }
         });
-
+        //Upon release find where it will snap to and determine whether that is a good location or not
         pieceView.setOnMouseReleased(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                System.out.println("release");
+                findSnapTo(pieceView);
+            }
+        });
+
+        //Move piece on drag
+        pieceView.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                translatePiece(event,pieceView,relativeMouseClick);
             }
         });
 
@@ -118,11 +136,58 @@ public class Viewer extends Application {
 
     }
 
+    void translatePiece(MouseEvent event,ImageView pieceView, double[] relativePoint){
+
+        //Move piece to mouse considering where the mouse was clicked inside the piece
+        pieceView.setX(event.getX()-relativePoint[0]);
+        pieceView.setY(event.getY()-relativePoint[1]);
+
+    }
+
+    void findSnapTo(ImageView pieceView){
+
+        int xSnap = -1000;
+        int ySnap = -1000;
+
+        double checkLength = SQUARE_SIZE / 2;
+
+        for(int i = 0; i <= 7 ; i++){
+
+            double xLine = (i * SQUARE_SIZE - pieceView.getX());
+
+            if(xLine <= checkLength && xLine >= -checkLength){
+                xSnap = i * SQUARE_SIZE;
+                break;
+            }
+
+        }
+
+        for(int i = 0; i <= 3 ; i++){
+
+            double yLine = (i * SQUARE_SIZE - pieceView.getY());
+
+            if(yLine <= checkLength && yLine >= -checkLength){
+                ySnap = i * SQUARE_SIZE;
+                break;
+            }
+
+
+        }
+
+        pieceView.setX(xSnap);
+        pieceView.setY(ySnap);
+
+    }
+
+
+
+
+
     void rotateAndFlip(ImageView pieceView,int orientation){
 
         //flip the piece over if needed
         if(orientation > 3){
-            pieceView.setScaleY(pieceView.getScaleY() * -1);
+            pieceView.setScaleY(-1);
             orientation = orientation - 4;
         }
 
@@ -131,10 +196,10 @@ public class Viewer extends Application {
         //correct x y positioning due to orientation change, this assumes that all pieces fit in a rectangle
         if(orientation % 2 != 0){
 
-            float adjust = (float)SQUARE_SIZE / 100;
+            //float adjust = (float)SQUARE_SIZE / 100;
 
-            double width = (pieceView.getImage().getWidth()) * adjust;
-            double height = pieceView.getImage().getHeight() * adjust;
+            double width = (pieceView.getImage().getWidth());// * adjust;
+            double height = pieceView.getImage().getHeight();// * adjust;
             double correction = (height - width) / 2;
 
             pieceView.setX(pieceView.getX() + correction);
@@ -143,8 +208,6 @@ public class Viewer extends Application {
 
 
     }
-
-
 
     /**
      * Create a basic text field for input and a refresh button.
