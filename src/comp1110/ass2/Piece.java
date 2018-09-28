93bc4940077bf75dbd91f15bef550f0c92596ddf
James <u6079329@anu.edu.au>
2018-09-28 12:02:08 +1000

    added to Piece

--- src/comp1110/ass2/Piece.java
+++ src/comp1110/ass2/Piece.java
@@ -63,6 +63,32 @@ public enum Piece {
         relativeXY = intialXY[id - 'a'];
     }
 
+    public static Piece getPiece(char id){
+        Piece piece = PIECEa;
+
+        switch (id){
+            case 'a':
+                return PIECEa;
+            case 'b':
+                return PIECEb;
+            case 'c':
+                return PIECEc;
+            case 'd':
+                return PIECEd;
+            case 'e':
+                return PIECEe;
+            case 'f':
+                return PIECEf;
+            case 'g':
+                return PIECEg;
+
+        }
+
+        return null;
+    }
+
+
+
     public int getColumn() {
         return column;
     }
